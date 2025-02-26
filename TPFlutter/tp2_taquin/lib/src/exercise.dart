511793c04309fe40f8b13ce654a5e8d88fc8f436
GaelGuillot <gael.guillot06@gmail.com>
2025-02-26 10:15:18 +0100

    Added Exo4, changed Exercise class to contain Widget (instead of StatefulWidget)

--- TPFlutter/tp2_taquin/lib/src/exercise.dart
+++ TPFlutter/tp2_taquin/lib/src/exercise.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Exercise {
   final int id;
   final String title;
-  final StatefulWidget widget;
+  final Widget widget;
 
   Exercise(this.id, this.title, this.widget);
 }
