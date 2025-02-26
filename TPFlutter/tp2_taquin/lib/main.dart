9e8c34bc41a9e10c2c21f225a83df92b656ccd51
Denis <dagbertdenis@gmail.com>
2025-02-26 09:42:11 +0100

    Emptied main

--- TPFlutter/tp2_taquin/lib/main.dart
+++ TPFlutter/tp2_taquin/lib/main.dart
@@ -32,10 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _sliderX = 0.5;
-  double _sliderZ = 0.5;
-  double _sliderS = 0.5;
-  bool? _mirrorBool = false;
 
   @override
   Widget build(BuildContext context) {
@@ -50,81 +46,10 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: 
-        Column(
+        ListView(
           children: [
-            
-            ClipRect(
-              child: Container(
-                height:300,
-                width:300,
-                child: Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()
-                  ..rotateZ(2*math.pi*_sliderZ)
-                  ..rotateX(2*math.pi*_sliderX)
-                  ..scale(2*_sliderS),
-              
-                  child: Transform.flip(
-                    flipX: _mirrorBool == true,
-                    child: const Image(
-                      height: 300,
-                      image: NetworkImage('https://picsum.photos/512/1024'),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Rotate X'),
-                Slider(value: _sliderX, onChanged: (double value) {
-                  setState(() {
-                    _sliderX = value;
-                  });
-                }),
-              ],
-            ),
-
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Rotate Z'),
-                Slider(value: _sliderZ, onChanged: (double value) {
-                  setState(() {
-                    _sliderZ = value;
-                  });
-                }),
-              ],
-            ),
-
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Scale'),
-                Slider(value: _sliderS, onChanged: (double value) {
-                  setState(() {
-                    _sliderS = value;
-                  });
-                }),
-              ],
-            ),
-
-           Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text('Mirror'),
-              Checkbox(
-                tristate: false,
-                value: _mirrorBool, 
-                onChanged: (bool? value) {
-                setState(() {
-                  _mirrorBool = value;
-                });
-              })
-           ],)
-          ],
+            ListTile()
+          ]
         )
       ),
     );
