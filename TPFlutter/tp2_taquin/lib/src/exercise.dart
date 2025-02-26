c14ad98ec94d5a2c2e49fcb89e1e921530fea697
GaelGuillot <gael.guillot06@gmail.com>
2025-02-26 09:25:25 +0100

    Created exercise class

---
+++ TPFlutter/tp2_taquin/lib/src/exercise.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Exercise {
+  final int id;
+  final String title;
+  final String desciption;
+  final StatefulWidget widget;
+
+  Exercise(this.id, this.title, this.desciption, this.widget);
+}
