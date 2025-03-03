d5005042f04dfd5fd1530379c514698ced50d46d
GaelGuillot <gael.guillot06@gmail.com>
2025-03-03 14:43:26 +0100

    Added variable grid size to exo5

--- TPFlutter/tp2_taquin/lib/src/screens/exo5_screen.dart
+++ TPFlutter/tp2_taquin/lib/src/screens/exo5_screen.dart
@@ -1,77 +1,75 @@
 import 'package:flutter/material.dart';
 
-class Exo5Screen extends StatelessWidget {
+class Exo5Screen extends StatefulWidget {
+  @override
+  _Exo5ScreenState createState() => _Exo5ScreenState();
+}
+
+class _Exo5ScreenState extends State<Exo5Screen> {
   final String title = 'Display grid';
+  double _sliderSize = 0.5;
+
+  int _calculateGridSize(double sliderValue) {
+    return (sliderValue * 10).toInt() + 1; 
+  }
 
   @override
   Widget build(BuildContext context) {
+    int gridSize = _calculateGridSize(_sliderSize);
+    double cellSize = 512 / gridSize;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Display grid'),
         centerTitle: true,
       ),
-      body: GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(20),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      crossAxisCount: 3,
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[100],
-          child: const Text("He'd have you all unravel at the"),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[200],
-          child: const Text('Heed not the rabble'),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[300],
-          child: const Text('Sound of screams but the'),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[400],
-          child: const Text('Who scream'),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[500],
-          child: const Text('Revolution is coming...'),
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[600],
-          child: const Text('Revolution, they...'),
-        ),
-                Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[700],
-          child: const Text('Revolution, they...'),
-        ),
-                Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[800],
-          child: const Text('Revolution, they...'),
-        ),
-                Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[900],
-          child: const Text('Revolution, they...'),
-        ),
-      ],
-    )
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(20),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: gridSize,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: gridSize * gridSize,
+              itemBuilder: (context, index) {
+                int row = index ~/ gridSize;
+                int col = index % gridSize;
+                return Container(
+                  width: cellSize,
+                  height: cellSize,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://picsum.photos/512/1024'),
+                      fit: BoxFit.cover,
+                      alignment: Alignment(
+                        -1 + 2 * col / (gridSize - 1),
+                        -1 + 2 * row / (gridSize - 1),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Size: '),
+              Slider(
+                value: _sliderSize,
+                onChanged: (double value) {
+                  setState(() {
+                    _sliderSize = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
-
-  /*Widget createTileWidgetFrom(Tile tile) {
-    return InkWell(
-      onTap: () {
-        print("tapped on tile");
-      },
-    );
-  }*/
 }
