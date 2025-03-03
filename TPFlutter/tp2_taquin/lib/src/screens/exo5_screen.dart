2cc4c5b92f58e46d98c6266c6b52b3802c930742
Denis <dagbertdenis@gmail.com>
2025-03-03 14:44:14 +0100

    Tried adding a picture in Exercice 5

--- TPFlutter/tp2_taquin/lib/src/screens/exo5_screen.dart
+++ TPFlutter/tp2_taquin/lib/src/screens/exo5_screen.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({required this.imageURL,required this.alignment});
+
+  Widget croppedImageTile() {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          child: Align(
+            alignment: this.alignment,
+            widthFactor: 0.3,
+            heightFactor: 0.3,
+            child: Image.network(this.imageURL),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Tile tile = Tile(
+//    imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
+
 class Exo5Screen extends StatelessWidget {
   final String title = 'Display grid';
+
+  Tile tile = Tile(
+    imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +46,15 @@ class Exo5Screen extends StatelessWidget {
       mainAxisSpacing: 10,
       crossAxisCount: 3,
       children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(8),
-          color: Colors.teal[100],
-          child: const Text("He'd have you all unravel at the"),
-        ),
+          SizedBox(
+            width: 150.0,
+            height: 150.0,
+            child: Container(
+                margin: EdgeInsets.all(20.0),
+                child: createTileWidgetFrom(tile)
+              )
+            ),
+
         Container(
           padding: const EdgeInsets.all(8),
           color: Colors.teal[200],
@@ -67,11 +100,11 @@ class Exo5Screen extends StatelessWidget {
     );
   }
 
-  /*Widget createTileWidgetFrom(Tile tile) {
+  Widget createTileWidgetFrom(Tile tile) {
     return InkWell(
       onTap: () {
         print("tapped on tile");
       },
     );
-  }*/
+  }
 }
